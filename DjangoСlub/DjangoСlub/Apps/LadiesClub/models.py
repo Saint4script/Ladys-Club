9e6fd72d69f1f8c98be33e6aea725be60be0c900
Saint4script <saint.4.script@gmail.com>
2021-01-18 20:31:16 +0800

    shit fixes

--- DjangoСlub/DjangoСlub/Apps/LadiesClub/models.py
+++ DjangoСlub/DjangoСlub/Apps/LadiesClub/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class News(models.Model):
     news_header = models.CharField(verbose_name = 'Заголовок', max_length = 100, blank=False)
-    news_text = models.TextField(verbose_name = 'Основной текст', max_length = 125, blank=False)
+    news_text = models.TextField(verbose_name = 'Основной текст', max_length = 170, blank=False)
     news_dates = models.TextField(verbose_name = 'Даты проведения', blank=False)
     news_image = models.ImageField(verbose_name = 'Превью', upload_to='images/', blank=False)
 
