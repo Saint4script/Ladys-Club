81569f3f357e94e3bb98ca53639c99ef981fed3d
Saltuide <goonerus38@gmail.com>
2021-02-10 18:12:15 +0800

    news pictures resize

--- DjangoСlub/DjangoСlub/Apps/LadiesClub/static/js/index.js
+++ DjangoСlub/DjangoСlub/Apps/LadiesClub/static/js/index.js
@@ -1,55 +1,67 @@
-function sendForm(){
+function sendForm() {
     let fd = new FormData(registration_form);
     // let loadGif = $.getElementByClass("gif-container")[0];
-    $('.gif-container').css('display','block');
-    $('.gif-shadow').css('display','block');
-    $('.fieldset')[0].disabled=true;
+    $('.gif-container').css('display', 'block');
+    $('.gif-shadow').css('display', 'block');
+    $('.fieldset')[0].disabled = true;
     $.ajax({
-        type: "POST",
-        url: '/send_form/',
-        data: fd,
-        contentType: false,
-        processData: false
-    }).done(function (result) {
-      $('.gif-container').css('display','none');
-      $('.gif-shadow').css('display','none');
-      $('.fieldset')[0].disabled=false;
-        if (result["status"] == "Ok"){
-            alert("Создали");      
-        }else{
-            alert("что-то пошло не так");
-        }
+            type: "POST",
+            url: '/send_form/',
+            data: fd,
+            contentType: false,
+            processData: false
+        }).done(function(result) {
+            $('.gif-container').css('display', 'none');
+            $('.gif-shadow').css('display', 'none');
+            $('.fieldset')[0].disabled = false;
+            if (result["status"] == "Ok") {
+                alert("Создали");
+            } else {
+                alert("что-то пошло не так");
+            }
+        })
+        .fail(function(jqXHR, exception) {
+            // Our error logic here
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            console.log(msg);
+        });
+}
+
+function setSizeForNewsImages() {
+    let imgDiv = $(".image")[0];
+    let newsImages = $(".image>img");
+    newsImages.each(function() {
+        $(this).css("width", imgDiv.offsetWidth);
+        $(this).css("height", imgDiv.offsetHeight);
+
     })
-    .fail(function (jqXHR, exception) {
-        // Our error logic here
-        var msg = '';
-        if (jqXHR.status === 0) {
-            msg = 'Not connect.\n Verify Network.';
-        } else if (jqXHR.status == 404) {
-            msg = 'Requested page not found. [404]';
-        } else if (jqXHR.status == 500) {
-            msg = 'Internal Server Error [500].';
-        } else if (exception === 'parsererror') {
-            msg = 'Requested JSON parse failed.';
-        } else if (exception === 'timeout') {
-            msg = 'Time out error.';
-        } else if (exception === 'abort') {
-            msg = 'Ajax request aborted.';
-        } else {
-            msg = 'Uncaught Error.\n' + jqXHR.responseText;
-        }
-        console.log(msg);
-    });
 }
 
 
-$(document).ready(function() {  
-    let form  = document.getElementById("registration_form");
+$(document).ready(function() {
+    // code for form sending
+    let form = document.getElementById("registration_form");
     var csrftoken = form.elements.csrfmiddlewaretoken.value;
 
     if ($(".gallery-cell").length == 0) {
-      $(".news-wrapper").css("display", "none")
+        $(".news-wrapper").css("display", "none")
     }
+
     function csrfSafeMethod(method) {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
@@ -62,5 +74,11 @@ $(document).ready(function() {
             }
         }
     });
-    
+
+    setSizeForNewsImages();
+
 });
+
+$(window).resize(() => {
+    setSizeForNewsImages();
+})
